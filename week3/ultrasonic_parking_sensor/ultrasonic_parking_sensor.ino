8942079279fe075e3ada29a35340361b3d6db6dd
Ayush Sharma <ayushsharma191207@gmail.com>
2026-07-12 22:22:04 +0530

    Update ultrasonic_parking_sensor.ino

--- week3/ultrasonic_parking_sensor/ultrasonic_parking_sensor.ino
+++ week3/ultrasonic_parking_sensor/ultrasonic_parking_sensor.ino
@@ -1,31 +1,13 @@
-/*
----------------------------------------------------------
-Project : Ultrasonic Parking Sensor
-Author  : Ayush Sharma
----------------------------------------------------------
-*/
-
-const int trigPin = 9;
-const int echoPin = 10;
-
-const int greenLED = 2;
-const int yellowLED = 3;
-const int redLED = 4;
-
+const int trigPin = 10;
+const int echoPin = 9;
 const int buzzer = 8;
 
-unsigned long previousMillis = 0;
-bool toggleState = false;
+long duration;
+float distance;
 
 void setup() {
-
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-
-  pinMode(greenLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
-  pinMode(redLED, OUTPUT);
-
   pinMode(buzzer, OUTPUT);
 
   Serial.begin(9600);
@@ -33,7 +15,7 @@ void setup() {
 
 void loop() {
 
-  // Measure Distance
+  // Send ultrasonic pulse
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
 
@@ -41,86 +23,32 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  long duration = pulseIn(echoPin, HIGH);
+  // Read echo
+  duration = pulseIn(echoPin, HIGH);
 
-  float distance = (duration * 0.034) / 2;
+  // Convert time to distance
+  distance = duration * 0.0343 / 2;
 
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
 
-  unsigned long currentMillis = millis();
-
-  // Reset outputs
-  digitalWrite(greenLED, LOW);
-  digitalWrite(yellowLED, LOW);
-  digitalWrite(redLED, LOW);
-  noTone(buzzer);
-
-  //---------------- SAFE ----------------
   if (distance > 50) {
-
-    digitalWrite(greenLED, HIGH);
-
-    Serial.println("SAFE");
+    noTone(buzzer);                 // Silent
   }
-
-  //---------------- WARNING ----------------
-  else if (distance > 20 && distance <= 50) {
-
-    digitalWrite(yellowLED, HIGH);
-
-    if (currentMillis - previousMillis >= 500) {
-
-      previousMillis = currentMillis;
-
-      toggleState = !toggleState;
-
-      if (toggleState)
-        tone(buzzer, 1000);
-      else
-        noTone(buzzer);
-    }
-
-    Serial.println("WARNING");
-  }
-
-  //---------------- DANGER ----------------
-  else if (distance > 10 && distance <= 20) {
-
-    digitalWrite(redLED, HIGH);
-
-    if (currentMillis - previousMillis >= 200) {
-
-      previousMillis = currentMillis;
-
-      toggleState = !toggleState;
-
-      if (toggleState)
-        tone(buzzer, 1000);
-      else
-        noTone(buzzer);
-    }
-
-    Serial.println("DANGER");
-  }
-
-  //---------------- STOP ----------------
-  else {
-
-    if (currentMillis - previousMillis >= 100) {
-
-      previousMillis = currentMillis;
-
-      toggleState = !toggleState;
-
-      digitalWrite(greenLED, toggleState);
-      digitalWrite(yellowLED, toggleState);
-      digitalWrite(redLED, toggleState);
-    }
-
+  else if (distance > 20) {
     tone(buzzer, 1000);
-
-    Serial.println("STOP");
+    delay(500);
+    noTone(buzzer);
+    delay(500);
+  }
+  else if (distance > 10) {
+    tone(buzzer, 1000);
+    delay(200);
+    noTone(buzzer);
+    delay(200);
+  }
+  else {
+    tone(buzzer, 1000);             // Continuous beep
   }
 }
